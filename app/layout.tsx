c6ec3816cbcc1381c195ded5630c4e9a51443527
andy90d <163924147+andy90d@users.noreply.github.com>
2026-06-11 17:11:03 +0000

    ioubenda integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Fraunces, DM_Sans } from "next/font/google";
+import Script from "next/script"; // <-- Importiamo il componente corretto di Next.js
 
 const fraunces = Fraunces({
   subsets: ["latin"],
@@ -23,7 +24,22 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="it" className={`${fraunces.variable} ${dmSans.variable}`}>
       <head>
-        <script type="text/javascript" src="https://embeds.iubenda.com/widgets/f2cc20d5-bfbe-4c85-880d-6c958cd3c60c.js"></script>
+        {/* 1. Script di configurazione per inizializzare l'oggetto Iubenda prima del widget */}
+        <Script id="iubenda-config" strategy="beforeInteractive">
+          {`
+            var _iub = _iub || [];
+            _iub.csConfiguration = {
+              "askForConsent": true,
+              "lang": "it"
+            };
+          `}
+        </Script>
+        
+        {/* 2. Caricamento del widget reale (Senza virgole di sintassi errate e con chiusura corretta) */}
+        <Script 
+          src="https://embeds.iubenda.com/widgets/f2cc20d5-bfbe-4c85-880d-6c958cd3c60c.js" 
+          strategy="afterInteractive"
+        />
       </head>
       <body>
         {children}
